b39a0a1cb04a81f554d4177d7edd1a4a8e4a9924
Hawk Viper <hsgrewal.22@gmail.com>
2018-03-11 21:29:10 +0530

    Coomit from VSCode

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var noOfClosingBrackets = 0;
 var kString = "";
 var iString = "";
 var dString = "";
+var lastkString = "";
 
 function parseNumber(ch) {
     leftOfCursor += ch;
     iString += ch;
     kString = leftOfCursor + rightOfCursor;;
     dString = leftOfCursor + "   " + rightOfCursor;
-    katex.render(kString, kScreen);
+    katexRender(kString, kScreen);
     document.getElementById("iScreen").innerHTML = iString;
     document.getElementById("dScreen").innerHTML = dString;
 }
@@ -29,22 +30,60 @@ function parseLetter(ch) {
     dString = leftOfCursor + "   " + rightOfCursor;
     document.getElementById("iScreen").innerHTML = iString;
     document.getElementById("dScreen").innerHTML = dString;
-    try {
-        katex.render(kString, kScreen);
-    }
-    catch(e)
-    {
-        console.log(e);
-    }
-    katex.render(kString, kScreen);
+    katexRender(kString, kScreen);
 }
+
+function parseDivivde() {
+    leftOfCursor = "";
+    rightOfCursor = "";
+    noOfClosingBrackets = 0;
+    kString = "0";
+    iString = "0";
+    dString = "0";
+    lastkString = "0";
+    document.getElementById("iScreen").innerHTML = iString;
+    document.getElementById("dScreen").innerHTML = dString;
+    katexRender(kString, kScreen);
+}
+
+function parseCE() {
+    leftOfCursor = "";
+    rightOfCursor = "";
+    noOfClosingBrackets = 0;
+    kString = "0";
+    iString = "0";
+    dString = "0";
+    lastkString = "0";
+    document.getElementById("iScreen").innerHTML = iString;
+    document.getElementById("dScreen").innerHTML = dString;
+    katexRender(kString, kScreen);
+}
+
+function katexRender(kStringin, kScreenin) {
+    iserror = false;
+    try {
+        katex.render(kStringin, kScreenin);
+    }
+    catch (e) {
+        iserror = true;
+    }
+    if (!iserror) {
+        lastkString = kStringin;
+    }
+    else {
+        katex.render(lastkString, kScreenin);
+    }
+
+}
+
+
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.js')
-    .then(function(registration) {
-      console.log('Registration successful, scope is:', registration.scope);
-    })
-    .catch(function(error) {
-      console.log('Service worker registration failed, error:', error);
-    });
-  }
-  
+        .then(function (registration) {
+            console.log('Registration successful, scope is:', registration.scope);
+        })
+        .catch(function (error) {
+            console.log('Service worker registration failed, error:', error);
+        });
+}
